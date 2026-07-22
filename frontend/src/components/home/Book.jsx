92fa6107bd451f897de0d64086f903d0f7a4d50b
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-07-22 15:43:23 +0800

    Booty shake

--- frontend/src/components/home/Book.jsx
+++ frontend/src/components/home/Book.jsx
@@ -18,11 +18,11 @@ export default function Book() {
   const searchAvailableBuses = () => {
     if (!destination || !date) {
       setError(true);
-      setShake(false);
+      setShake(true);
 
       setTimeout(() => {
-        setShake(true);
-      }, 10);
+        setShake(false);
+      }, 1000);
 
       return;
     }
@@ -40,166 +40,138 @@ export default function Book() {
 
   useEffect(() => {
     getRoutes().then((data) =>
-        setDestinations(data.map((route) => route.destination))
+      setDestinations(data.map((route) => route.destination)),
     );
   }, []);
 
   const filteredDestinations = useMemo(() => {
     return destinations.filter((place) =>
-        place.toLowerCase().includes(destination.toLowerCase())
+      place.toLowerCase().includes(destination.toLowerCase()),
     );
   }, [destinations, destination]);
 
   return (
-      <>
-        <h3>Plan Your Trip</h3>
-        <p>Find the next available bus to your destination.</p>
+    <>
+      <h3>Plan Your Trip</h3>
+      <p>Find the next available bus to your destination.</p>
 
-        <div className="trip-type">
-          One-Way
-        </div>
+      <div className="trip-type">One-Way</div>
 
-        <div className="form-fields">
-
-          <div className="inline-input">
-
-            <div className="field">
-              <div className="child-field">
-                <label>FROM</label>
-                <input
-                    type="text"
-                    value="Cebu South Terminal"
-                    disabled
-                />
-              </div>
+      <div className="form-fields">
+        <div className="inline-input">
+          <div className="field">
+            <div className="child-field">
+              <label>FROM</label>
+              <input type="text" value="Cebu South Terminal" disabled />
             </div>
-
-            <div className="oneway-icon">
-              <img src={oneway} alt="oneway" />
-            </div>
-
-            <div className={`field destination-field ${
-                error && !destination ? "input-error" : ""
-            } ${shake && !destination ? "shake" : ""}`}>
-
-              <div className="child-field">
-                <label>TO</label>
-
-                <input
-                    type="text"
-                    placeholder="Destination stop"
-                    value={destination}
-                    onFocus={() => setShowList(true)}
-                    onChange={(e) => {
-                      setDestination(e.target.value);
-                      setError(false);
-                    }}
-                />
-
-                {showList && (
-                    <div className="destination-list">
-                      {filteredDestinations.map((place, index) => (
-                          <p
-                              key={index}
-                              onClick={() => {
-                                setDestination(place);
-                                setShowList(false);
-                                setError(false);
-                              }}
-                          >
-                            {place}
-                          </p>
-                      ))}
-                    </div>
-                )}
-
-              </div>
-
-            </div>
-
           </div>
 
+          <div className="oneway-icon">
+            <img src={oneway} alt="oneway" />
+          </div>
 
-          <div className="inline-input">
+          <div
+            className={`field destination-field ${
+              error && !destination ? "input-error" : ""
+            } ${shake && !destination ? "shake" : ""}`}
+          >
+            <div className="child-field">
+              <label>TO</label>
 
-            <div className={`field ${
-                error && !date ? "input-error" : ""
-            } ${shake && !date ? "shake" : ""}`}>
-
-              <img
-                  src="/src/assets/ic_calendar.png"
-                  className="form-logo"
-                  alt="calendar"
+              <input
+                type="text"
+                placeholder="Destination stop"
+                value={destination}
+                onFocus={() => setShowList(true)}
+                onChange={(e) => {
+                  setDestination(e.target.value);
+                  setError(false);
+                }}
               />
 
-              <div className="child-field">
-                <label>DATE</label>
-
-                <input
-                    type="date"
-                    value={date}
-                    min={new Date().toISOString().split("T")[0]}
-                    onChange={(e) => {
-                      setDate(e.target.value);
-                      setError(false);
-                    }}
-                />
-              </div>
-
-            </div>
-
-
-            <div className="field">
-
-              <img
-                  src="/src/assets/passenger.png"
-                  className="form-logo"
-                  alt="passenger"
-              />
-
-              <div className="child-field">
-
-                <label>PASSENGER</label>
-
-                <div className="counter">
-
-                  <button
-                      onClick={() =>
-                          passenger > 1 &&
-                          setPassenger(passenger - 1)
-                      }
-                  >
-                    -
-                  </button>
-
-                  <span>{passenger}</span>
-
-                  <button
-                      onClick={() =>
-                          passenger < 5 &&
-                          setPassenger(passenger + 1)
-                      }
-                  >
-                    +
-                  </button>
-
+              {showList && (
+                <div className="destination-list">
+                  {filteredDestinations.map((place, index) => (
+                    <p
+                      key={index}
+                      onClick={() => {
+                        setDestination(place);
+                        setShowList(false);
+                        setError(false);
+                      }}
+                    >
+                      {place}
+                    </p>
+                  ))}
                 </div>
-
-              </div>
-
+              )}
             </div>
-
           </div>
-
         </div>
 
+        <div className="inline-input">
+          <div
+            className={`field ${
+              error && !date ? "input-error" : ""
+            } ${shake && !date ? "shake" : ""}`}
+          >
+            <img
+              src="/src/assets/ic_calendar.png"
+              className="form-logo"
+              alt="calendar"
+            />
 
-        <button
-            className="search-button"
-            onClick={searchAvailableBuses}
-        >
-          Search Available Buses
-        </button>
-      </>
+            <div className="child-field">
+              <label>DATE</label>
+
+              <input
+                type="date"
+                value={date}
+                min={new Date().toISOString().split("T")[0]}
+                onChange={(e) => {
+                  setDate(e.target.value);
+                  setError(false);
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <img
+              src="/src/assets/passenger.png"
+              className="form-logo"
+              alt="passenger"
+            />
+
+            <div className="child-field">
+              <label>PASSENGER</label>
+
+              <div className="counter">
+                <button
+                  onClick={() => passenger > 1 && setPassenger(passenger - 1)}
+                >
+                  -
+                </button>
+
+                <span>{passenger}</span>
+
+                <button
+                  onClick={() => passenger < 5 && setPassenger(passenger + 1)}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <button
+        className={`search-button && ${error && shake ? "shake" : ""}`}
+        onClick={searchAvailableBuses}
+      >
+        Search Available Buses
+      </button>
+    </>
   );
 }
